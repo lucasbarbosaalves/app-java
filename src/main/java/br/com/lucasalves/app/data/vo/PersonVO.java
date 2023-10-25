58a0af8e4ffdf4c50c3230ce5932ae6d2fb9de48
lukas <lkab05@hotmail.com>
2023-10-25 16:11:28 -0300

    refactoring Person VO

--- src/main/java/br/com/lucasalves/app/data/vo/PersonVO.java
+++ src/main/java/br/com/lucasalves/app/data/vo/PersonVO.java
@@ -1,34 +1,19 @@
 package br.com.lucasalves.app.data.vo;
 
-import jakarta.persistence.*;
-
 import java.io.Serializable;
-import java.util.Objects;
 
-@Entity
-@Table(name = "person")
+
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @Column(name = "first_name", nullable = false, length = 80)
     private String firstName;
-
-    @Column(name = "last_name", nullable = false, length = 80)
     private String lastName;
-
-    @Column(nullable = false, length = 100)
     private String address;
-
-    @Column(nullable = false, length = 6)
     private String gender;
 
-    public PersonVO() {
-    }
+    public PersonVO() {}
 
     public Long getId() {
         return id;
@@ -69,7 +54,6 @@ public class PersonVO implements Serializable {
     public void setGender(String gender) {
         this.gender = gender;
     }
-
     @Override
     public int hashCode() {
         return super.hashCode();
