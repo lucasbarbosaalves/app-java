3fb000ca144897f9185b0a60441b341349b6189c
lukas <lkab05@hotmail.com>
2023-10-25 17:04:27 -0300

    add custom JSON serialization in VO

--- src/main/java/br/com/lucasalves/app/data/vo/PersonVO.java
+++ src/main/java/br/com/lucasalves/app/data/vo/PersonVO.java
@@ -1,14 +1,19 @@
 package br.com.lucasalves.app.data.vo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.io.Serializable;
 
-
+@JsonPropertyOrder({"id", "first_name", "last_name", "address", "gender"})
 public class PersonVO implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     private Long id;
+    @JsonProperty("first_name")
     private String firstName;
+    @JsonProperty("last_name")
     private String lastName;
     private String address;
     private String gender;
