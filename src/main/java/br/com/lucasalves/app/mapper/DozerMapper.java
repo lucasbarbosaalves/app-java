af785cc684d5e1a9a7625a8889af7a5d1f21ba21
lukas <lkab05@hotmail.com>
2023-10-25 15:38:27 -0300

    add DozerMapper and configuration bean

---
+++ src/main/java/br/com/lucasalves/app/mapper/DozerMapper.java
@@ -0,0 +1,25 @@
+package br.com.lucasalves.app.mapper;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DozerMapper {
+
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <O, D> D parseObject(O origin, Class<D> destination) {
+        return mapper.map(origin, destination);
+    }
+
+    public static <O, D> List<D> parseObject(List<O> origin, Class<D> destination) {
+        List<D> destionationObjects = new ArrayList<D>();
+
+        for(O o : origin) {
+            destionationObjects.add(mapper.map(o, destination));
+        }
+        return destionationObjects;
+    }
+}
