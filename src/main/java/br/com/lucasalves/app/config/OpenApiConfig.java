77b62dc112e7ea182b1b0a296e77567b60d8652b
lukas <lkab05@hotmail.com>
2023-10-26 14:28:29 -0300

    add basic config of Swagger(Open API)

---
+++ src/main/java/br/com/lucasalves/app/config/OpenApiConfig.java
@@ -0,0 +1,22 @@
+package br.com.lucasalves.app.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("RESTful API with Java 17 and Spring Boot 3")
+                        .version("v1")
+                        .description("")
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+}
