3279f2069b3f709f134e721a93e494dd983a71b7
lukas <lkab05@hotmail.com>
2023-10-26 12:49:08 -0300

    add yaml converter serialization

---
+++ src/main/java/br/com/lucasalves/app/serialization/converter/YamlJacksonConverter.java
@@ -0,0 +1,13 @@
+package br.com.lucasalves.app.serialization.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public class YamlJacksonConverter extends AbstractJackson2HttpMessageConverter{
+
+        public YamlJacksonConverter() {
+            super(new YAMLMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL), MediaType.parseMediaType("application/x-yaml"));
+        }
+}
