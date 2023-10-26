f10ae040c3539a885082f07a31bf0d0fca8e29e9
lukas <lkab05@hotmail.com>
2023-10-26 14:58:18 -0300

    created book model layer

---
+++ src/main/java/br/com/lucasalves/app/model/Book.java
@@ -0,0 +1,87 @@
+package br.com.lucasalves.app.model;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "books")
+public class Book implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 180)
+    private String author;
+
+    @Column(name = "lauch_date", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date lauchDate;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Date getLauchDate() {
+        return lauchDate;
+    }
+
+    public void setLauchDate(Date lauchDate) {
+        this.lauchDate = lauchDate;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(id, book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
