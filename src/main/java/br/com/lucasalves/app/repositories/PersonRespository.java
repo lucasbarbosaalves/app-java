fa307a81a05fe97409f8e085e5d9279a17e07023
lukas <lkab05@hotmail.com>
2023-10-28 14:02:42 -0300

    implementing custom query findPersonsByNames in repository

--- src/main/java/br/com/lucasalves/app/repositories/PersonRespository.java
+++ src/main/java/br/com/lucasalves/app/repositories/PersonRespository.java
@@ -1,6 +1,7 @@
 package br.com.lucasalves.app.repositories;
 
 import br.com.lucasalves.app.model.Person;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -11,4 +12,7 @@ public interface PersonRespository extends JpaRepository<Person, Long> {
     @Modifying
     @Query(value = "UPDATE Person p set p.enabled = false WHERE p.id =:id") // JPQL
     void disablePerson(@Param("id") Long id);
+
+    @Query(value = "SELECT p FROM Person p WHERE p.firstName LIKE LOWER(CONCAT('%' ,:firstName, '%'))") // JPQL
+    Page<Person> findPersonsByNames(@Param("firstName") String firstName);
 }
