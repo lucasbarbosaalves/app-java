74cfa27104608852358b37d4f8951dbbd60942a5
lukas <lkab05@hotmail.com>
2023-10-28 14:15:43 -0300

    adjust custom query findPersonsByNames in repository

--- src/main/java/br/com/lucasalves/app/repositories/PersonRespository.java
+++ src/main/java/br/com/lucasalves/app/repositories/PersonRespository.java
@@ -2,6 +2,7 @@ package br.com.lucasalves.app.repositories;
 
 import br.com.lucasalves.app.model.Person;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -14,5 +15,5 @@ public interface PersonRespository extends JpaRepository<Person, Long> {
     void disablePerson(@Param("id") Long id);
 
     @Query(value = "SELECT p FROM Person p WHERE p.firstName LIKE LOWER(CONCAT('%' ,:firstName, '%'))") // JPQL
-    Page<Person> findPersonsByNames(@Param("firstName") String firstName);
+    Page<Person> findPersonsByName(@Param("firstName") String firstName, Pageable pageable);
 }
